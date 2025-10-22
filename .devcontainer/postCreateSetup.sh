78e31a99b24eba770c6ec518b9455557bde50bf4
Mark Chadwick <31071714+astorguy@users.noreply.github.com>
2025-10-22 18:48:27 +0000

    changed workspace to py4spice

--- .devcontainer/postCreateSetup.sh
+++ .devcontainer/postCreateSetup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-cp /workspaces/spicey01/dotfiles/vscode_bash_aliases /home/vscode/.bash_aliases
-cp /workspaces/spicey01/dotfiles/vscode_bashrc /home/vscode/.bashrc
+cp /workspaces/py4spice/dotfiles/vscode_bash_aliases /home/vscode/.bash_aliases
+cp /workspaces/py4spice/dotfiles/vscode_bashrc /home/vscode/.bashrc
 
 uv sync --locked
