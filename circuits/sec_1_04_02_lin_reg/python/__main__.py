7822570dc73b5c279cddc4d1630b9f0fe8f98074
Mark Chadwick <31071714+astorguy@users.noreply.github.com>
2025-10-25 00:18:08 +0000

    initial work on __main__.py

--- circuits/sec_1_04_02_lin_reg/python/__main__.py
+++ circuits/sec_1_04_02_lin_reg/python/__main__.py
@@ -1,5 +1,76 @@
+import tomllib
+from pathlib import Path
+
+import py4spice as spi
+
+CONFIG_FILENAME = Path("/workspaces/py4spice/circuits/config.toml")
+PROJECT_SECTION = "SEC_1_04_02"
+
+
+class Ky:
+    """Keys for dictionaries.  Defined here at top level so they can be
+    referenced instead of using strings for keys.
+    """
+
+    # Keys for decoding config file
+    GLOBAL = "GLOBAL"
+    PROJECT = "PROJECT"
+    NGSPICE_EXE_STR = "NGSPICE_EXE_STR"
+    NETLISTS_DIR_STR = "NETLISTS_DIR_STR"
+    RESULTS_DIR_STR = "RESULTS_DIR_STR"
+    SIM_TRANSCRIPT_STR = "SIM_TRANSCRIPT_STR"
+    PROJ_PATH_STR = "PROJ_PATH_STR"
+
+    # Keys for the paths_dict
+    NGSPICE_EXE = "ngspice_exe"
+    PROJ_PATH = "proj_path"
+    NETLISTS_PATH = "netlists_path"
+    RESULTS_PATH = "results_path"
+    SIM_TRANSCRIPT_FILENAME = "sim_transcript_filename"
+
+    # Keys for the netlists_dict
+    BLANKLINE = "blankline"
+    TITLE = "title"
+    END_LINE = "end_line"
+    LOAD = "load"
+    STIMULUS = "stimulus"
+    SUPPLIES = "supplies"
+    MODELS = "models"
+    DUT = "dut"
+    CONTROL = "control"
+    TOP1 = "top1"
+
+    # Keys for the vectors_dict
+    VEC_ALL = "vec_all"
+    VEC_ALL_EXPANDED = "vec_all_expanded"
+    VEC_OUT = "vec_out"
+    VEC_IN_OUT = "vec_in_out"
+    VEC_INTEREST = "vec_interest"
+    VEC_POWER_CALC = "vec_power_calc"
+    VEC_ETA = "vec_eta"
+
+
+def initialize() -> tuple[
+    dict[str, Path], dict[str, spi.Netlist], dict[str, spi.Vectors]
+]:
+    """Initialize paths, netlists, and vectors dictionaries for the project."""
+    with open(CONFIG_FILENAME, "rb") as config_file:
+        my_config = tomllib.load(config_file)
+
+    paths_dict = {}
+    netlists_dict = {}
+    vectors_dict = {}
+    return paths_dict, netlists_dict, vectors_dict
+
+
 def main():
-    print("sec_1_04_02_lin_reg")
+    # initialize paths, netlists, and vectors dictionaries
+    paths_dict, netlists_dict, vectors_dict = initialize()
+
+    # Part 1: Simulate and analyze
+    # note: we pass back the netlists_dict because the top1 netlist was added to it
+    netlists_dict = part1(paths_dict, netlists_dict, vectors_dict)
+
 
 if __name__ == "__main__":
     main()
