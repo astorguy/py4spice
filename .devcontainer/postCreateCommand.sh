c15d0a051a0a52e3bf6d1bb12e6558c4669aac18
Mark Chadwick <31071714+astorguy@users.noreply.github.com>
2025-11-14 05:04:40 +0000

    clean up

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 set -e
-
-# cp /workspaces/py4spice/dotfiles/vscode_bash_aliases /home/vscode/.bash_aliases
-# cp /workspaces/py4spice/dotfiles/vscode_bashrc /home/vscode/.bashrc
 uv sync --dev
